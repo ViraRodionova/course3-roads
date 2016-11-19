9f2fa32d4775326c9f8edcb11ae0b80cfed1bdad
ViraRodionova <bepa.rdnv@gmail.com>
2016-11-19 19:48:32 +0200

    changed route-table structure

--- roads/static/js/base.js
+++ roads/static/js/base.js
@@ -13,30 +13,34 @@ function formTableRows(routes) {
 
     console.log(tableData);
 
-    var count = 0;
+    // var count = 0;
     var html = "";
 
     if (tableData.length == 0) return html;
 
     tableData.forEach(function (item, i) {
-        count = item.ways.length;
+        // count = item.ways.length;
 
-        html += "<tr data-toggle=\"collapse\" data-target=\"#accordion"+i+"\" class=\"clickable\">" +
+        // html += "<tr data-toggle=\"collapse\" data-target=\"#accordion"+i+"\" class=\"clickable\">" +
+        //     "<td>"+ item.address.start +"</td>" +
+        //     "<td>"+ item.address.end +"</td>" +
+        //     "<td><span style='border-radius:25px;background-color:#c8e5bc;'>"+ count +"</span></td>" +
+        //     "</tr>" +
+        //     "<tr>" +
+        //     "<td colspan=\"3\">" +
+        //     "<div id=\"accordion"+i+"\" class=\"collapse\">" +
+        //     "<table class=\"table table-hover\">";
+
+        // item.ways.forEach(function (item1, j) {
+        //     html += "<tr><td>Шлях"+ (j+1) +"</td></tr>"
+        // });
+        // html += "</table></div></td></tr>";
+
+        html += "<tr>" +
             "<td>"+ item.address.start +"</td>" +
             "<td>"+ item.address.end +"</td>" +
-            "<td><span style='border-radius:25px;background-color:#c8e5bc;'>"+ count +"</span></td>" +
-            "</tr>" +
-            "<tr>" +
-            "<td colspan=\"3\">" +
-            "<div id=\"accordion"+i+"\" class=\"collapse\">" +
-            "<table class=\"table table-hover\">";
-
-        item.ways.forEach(function (item1, j) {
-            html += "<tr><td>Шлях"+ (j+1) +"</td></tr>"
-        });
-
-        html += "</table></div></td></tr>";
+            "</tr>";
     });
-    console.log(html);
+    // console.log(html);
     return html;
 }
